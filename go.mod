2633d4804de9ef247214445b52078db5927cd09b
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-07-16 10:22:17 +0900

    calback

--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module github.com/xxarupakaxx/twitterBot
 go 1.16
 
 require (
-	github.com/labstack/echo v3.3.10+incompatible // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
 	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
 )
--- main.go
+++ main.go
@@ -5,17 +5,28 @@ import (
 	"crypto/sha256"
 	"encoding/base64"
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"log"
+	"os"
 )
 
 func CreateCRCToken(crcToken string) string {
-	mac := hmac.New(sha256.New, []byte(cs)) //csはConsumer Secret
+	mac := hmac.New(sha256.New, []byte(os.Getenv("")))
 	mac.Write([]byte(crcToken))
 	return "sha256=" + base64.StdEncoding.EncodeToString(mac.Sum(nil))
 }
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("oioio")
+	}
+
 	e:=echo.New()
+	e.Use(middleware.Logger())
+
 	e.POST("/callback", func(c echo.Context) error {
 		return nil
 	})
+	e.Start(":"+port)
 }
